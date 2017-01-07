87bf7ad410fedde9b9e945f03b9306fe019d00ff
Rafsan Chowdhury <rafsanchowdhury@Rafsans-MacBook-Air.local>
2017-01-07 13:17:20 -0500

    Initial Commit

---
+++ PokeDex3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokeDex3
+//
+//  Created by Rafsan Chowdhury on 1/7/17.
+//  Copyright © 2017 Mcraf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
